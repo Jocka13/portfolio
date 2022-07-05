9f0d105b962b86efaed24663267d4a263f7bb2ae
Jocka13 <joce.velkovski33@gmail.com>
2022-07-05 16:11:32 +0200

    added more information in the body

--- src/components/Body.vue
+++ src/components/Body.vue
@@ -2,38 +2,46 @@
     <main>
       <section>
         <div class="container">
-          <div class="row row-cols-auto mt-3">
+          <div class="row row-cols-auto mx-auto mt-3">
              <div class="col">
                <img alt="jocka-jpg" src="../assets/images/jocka-image.jpg" width="120" height="120"/>
              </div>
              <div class="col">
                <h4>Jordan Velkovski</h4>
-               <span>Front-end Developer</span>
+               <span>Full-stack Developer</span>
              </div>
           </div>
-          <div class="row row-cols-auto mt-5">
-            <div class="col col-6">
+          <div class="row mt-5 ">
+            <div class="col col-11 ">
               <ul class="list-inline">
                 <li class="list-inline-item fs-6"><i class="fa fa-user fa-2x" aria-hidden="true"></i></li>
-                <li class="list-inline-item fs-4">Profile</li>
+                <li class="list-inline-item fs-4 fw-bolder">Profile</li>
               </ul>
             </div>
-            <div class="col col-11">
-              <p>I am a simple guy that wants to learn new stuff everyday.The digitalization is something powerful.</p>
+            <div class="col col-9">
+              <p>I am a simple guy that wants to learn new stuff everyday. I believe that everything is going to be degitialized soon.</p>
             </div>
           </div>
-
-          <div class="row row-cols-auto">
-            <div class="col col-6 col-sm-auto">
-              <ul class="list-inline">
+          <div class="row">
+            <div class="col col-11 mb-0">
+              <ul class="list-inline mb-0">
                 <li class="list-inline-item fa-2x fs-3"><i class="fa fa-suitcase" aria-hidden="true"></i></li>
-                <li class="list-inline-item fs-4">Employment history</li>
+                <li class="list-inline-item fs-4 fw-bolder">Employment history</li>
                 <h6>Participant at Bike Computer Co</h6>
+                <p class="mb-1"><small>July 2022 - Present</small></p>
               </ul>
+              <p class="fs-6">This is my first time beeing participant here in this company. For the first week l am very satisfied how the work is going. I haven't work anywhere else before.</p>
             </div>
           </div>
-
-       </div>
+          <div class="row mt-3">
+            <div class="col col-11">
+              <ul class="list-inline mb-0">
+                <h6>Full stack developer for my needs</h6>
+              </ul>
+              <p class="fs-6">I have made several project for my faculty that l go currently, but l haven't done big projects for some other companies.</p>
+            </div>
+          </div>
+        </div>
       </section>
     </main>
 </template>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,7 +2,7 @@
   <main>
     <nav class="navbar navbar-light bg-light">
       <div class="container-fluid">
-        <span class="navbar-brand mx-auto">Welcome to my first portfolio</span>
+        <span class="navbar-brand mx-auto">Welcome to my portfolio</span>
       </div>
     </nav>
   </main>
